dde1002bd6a9d5893371d7c2ad9a0df253e4ad3e
Frederik Stobbe <frederik.stobbe@outlook.de>
2024-12-21 19:18:17 +0100

    move flutter app to new directory, basic camera support

--- android/app/src/main/kotlin/com/example/chesseboard/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.chesseboard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ app/android/app/src/main/java/com/chessapp/automated_chess_board_detection/MainActivity.java
@@ -0,0 +1,6 @@
+package com.chessapp.automated_chess_board_detection;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
